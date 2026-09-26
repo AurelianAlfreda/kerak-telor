071cdf9eeb6f257a8ae735504f85644c7f5d317b
chatrinatricica <chatrinatricia@gmail.com>
2026-09-26 07:18:18 +0700

    Pengenalan kerak telor

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+const mainNav = document.getElementById('mainNav');
 
+mobileMenuBtn.addEventListener('click', function () {
+    mainNav.classList.toggle('open');
+
+    const isOpen = mainNav.classList.contains('open');
+    mobileMenuBtn.setAttribute('aria-expanded', isOpen);
+});
+
+const navLinks = mainNav.querySelectorAll('.nav-link');
+navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+        mainNav.classList.remove('open');
+    });
+});
